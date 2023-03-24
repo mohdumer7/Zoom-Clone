c93cb4b76bc6da2e4fa2bc7a0b5419751f623256
mohdumer7 <umermohammed95@gmail.com>
2023-03-25 00:02:28 +0530

    Video Call now Working with fix error (1)

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -5,7 +5,8 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://zoom-clone-lxry.onrender.com");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
